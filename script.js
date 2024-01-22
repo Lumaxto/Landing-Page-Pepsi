a3744e6351c9114dbf14d4c1439cc96cabc498ac
Lumaxto <maxilencina19@gmail.com>
2024-01-22 15:44:43 -0300

    se agrega un nuevo proyecto

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+function ImgSlider (pepsi){
+    document.querySelector('.pepsi-azul').src=pepsi;
+}
+
+function BgColor (color){
+let cbg =document.querySelector('section')
+cbg.style.background=color;
+}
